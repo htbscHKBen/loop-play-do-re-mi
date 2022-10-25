dfd87c76d953c6f64094b41021d5c073049ff082
htbscHKBen <116170916+htbscHKBen@users.noreply.github.com>
2022-10-25 22:04:58 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+})
